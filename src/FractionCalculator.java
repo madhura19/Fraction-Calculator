664c1490e1a407cba8abb811fdd60b4668689dfd
Madhura <madhuradaptardar@gmail.com>
2018-04-07 20:20:41 -0400

    Changes in validFraction()

--- src/FractionCalculator.java
+++ src/FractionCalculator.java
@@ -18,29 +18,45 @@ public class FractionCalculator {
             if (operand.equals("+")){
                 System.out.println(frac1 + " " + operand + " " + frac2 + " = ");
                 result = frac1.add(frac2);
+                if (result.getDenominator() == 1){
+                    System.out.println(result.getNumerator());
+                }
+                else
                 System.out.println(result.toString());
             }
 
             else if (operand.equals("-")){
                 System.out.println(frac1 + " " + operand + " " + frac2 + " = ");
                 result = frac1.subtract(frac2);
-                System.out.println(result.toString());
+                if (result.getDenominator() == 1){
+                    System.out.println(result.getNumerator());
+                }
+                else
+                    System.out.println(result.toString());
             }
 
             else if (operand.equals("*")){
                 System.out.println(frac1 + " " + operand + " " + frac2 + " = ");
                 result = frac1.multiply(frac2);
-                System.out.println(result.toString());
+                if (result.getDenominator() == 1){
+                    System.out.println(result.getNumerator());
+                }
+                else
+                    System.out.println(result.toString());
             }
 
             else if (operand.equals("/")){
                 System.out.println(frac1 + " " + operand + " " + frac2 + " = ");
                 result = frac1.divide(frac2);
-                System.out.println(result.toString());
+                if (result.getDenominator() == 1){
+                    System.out.println(result.getNumerator());
+                }
+                else
+                    System.out.println(result.toString());
             }
 
             else if (operand.equals("=")){
-                System.out.println(frac1 + " " + operand + " " + frac2 + " = ");
+                System.out.println(frac1 + " " + operand + " " + frac2 + " is ");
                 System.out.println(frac1.equals(frac2));
             }
 
@@ -71,10 +87,10 @@ public class FractionCalculator {
             String numerator = inputArray[0];
             String denominator = inputArray[1];
 
-            if (!isNumber(numerator) && !isNumber(denominator) && Integer.parseInt(denominator) == 0 && numerator.isEmpty() && denominator.isEmpty()){
-                return false;
+            if (isNumber(numerator) && isNumber(denominator) && Integer.parseInt(denominator) != 0 && !numerator.isEmpty() && !denominator.isEmpty()){
+                return true;
             }
-            else return true;
+            else return false;
         }
         else
         return (isNumber(input));
